eb27c8c7d6df508a4c662e7ffcf77e7ac7cadd74
imanurag17 <anuraag.cas@gmail.com>
2022-08-23 17:19:42 +0530

    Update NavBar.jsx

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
  class NavBar extends Component {
     state = {  } 
     render() { 
-        return (
+        return ( 
         <>
         <div className='nav_bar'>
             <div className='nav_img'>
